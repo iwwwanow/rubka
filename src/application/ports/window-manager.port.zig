7c02e0a109194676fb0b9816f979dc8054792fd1
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-07-31 18:52:42 +0300

    feat(application): wm port

--- src/application/ports/window-manager.port.zig
+++ src/application/ports/window-manager.port.zig
@@ -3,13 +3,9 @@ const Direction = enum { left, right };
 
 pub const WindowManagerPort = struct {
     ptr: *anyopaque,
-};
+    moveFn: *const fn (ptr: *anyopaque, direction: Direction) void,
 
-// const WindowManagerPort = struct {
-//     ptr: *anyopaque,
-//     moveFn: *const fn (ptr: *anyopaque, direction: Direction) void,
-//
-//     fn move(self: WindowManagerPort, direction: Direction) void {
-//         self.moveFn(self.ptr, direction);
-//     }
-// };
+    fn move(self: WindowManagerPort, direction: Direction) void {
+        self.moveFn(self.ptr, direction);
+    }
+};
