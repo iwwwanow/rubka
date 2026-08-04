c8954e28f8fca6c40d391ab926090f6abbbfd572
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-08-04 11:49:41 +0300

    feat(infrastruction): stub adapter

---
+++ src/infrastructure/adapters/stub.adapter.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+const port_mod = @import("../../application/ports/window-manager.port.zig");
+
+pub const StubWindowManagerAdapter = struct {};
+
+fn move_fn(ptr: *anyopaque, direction: port_mod.Direction) void {
+    const self: *StubWindowManagerAdapter = @ptrCast(ptr);
+    _ = self;
+
+    const direction_string: [:0]const u8 = @tagName(direction);
+    std.debug.print("{s}\n", .{direction_string});
+}
+
+pub fn wrap(self: *StubWindowManagerAdapter) port_mod.WindowManagerPort {
+    return .{ .ptr = self, .move_fn = move_fn };
+}
