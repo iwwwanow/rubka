1f0be5cf3757341316d7f63fc6a616dfa37be885
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-08-04 20:26:33 +0300

    feat(main): composition root init modules

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,15 @@
 const std = @import("std");
 const Io = std.Io;
 
+const adapter_mod = @import("./infrastructure/adapters/stub.adapter.zig");
+const use_case_mod = @import("./application/use-cases/move-window.use-case.zig");
+
 const iwwwanow_rubka = @import("iwwwanow_rubka");
 
 pub fn main() void {
+    var adapter_impl: adapter_mod.StubWindowManagerAdapter = .{};
+    const port_impl = adapter_mod.wrap(&adapter_impl);
+    const use_case_impl: use_case_mod.MoveWindowUseCase = .{ .window_manager = port_impl };
+
     iwwwanow_rubka.printHelloWorld();
 }
